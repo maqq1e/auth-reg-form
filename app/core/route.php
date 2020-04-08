2100c55449b671d79e42bc073bdfe7987084ba77
Daniel maqq1e Hramkov <daniel.hramkov@gmail.com>
2020-04-08 12:17:01 +0500

    Add core files

---
+++ app/core/route.php
@@ -0,0 +1,73 @@
+<?php
+class Route
+{
+	static function start()
+	{
+		// Controller and default action
+		$controller_name = 'Main';
+		$action_name = 'index';
+
+		$routes = explode('/', $_SERVER['REQUEST_URI']);
+
+		// Get controller name
+		if ( !empty($routes[1]) )
+		{
+			$controller_name = $routes[1];
+		}
+
+		// Get action name
+		if ( !empty($routes[2]) )
+		{
+			$action_name = $routes[2];
+		}
+
+		// Add prefix
+		$model_name = 'Model_'.$controller_name;
+		$controller_name = 'Controller_'.$controller_name;
+		$action_name = 'action_'.$action_name;
+
+		// Connect file with modal class ( if it is exist )
+
+		$model_file = strtolower($model_name).'.php';
+		$model_path = "application/models/".$model_file;
+		if(file_exists($model_path))
+		{
+			include "application/models/".$model_file;
+		}
+
+		// Connect file with controller class
+		$controller_file = strtolower($controller_name).'.php';
+		$controller_path = "application/controllers/".$controller_file;
+		if(file_exists($controller_path))
+		{
+			include "application/controllers/".$controller_file;
+		}
+		else
+		{
+			Route::ErrorPage404();
+		}
+
+		// Create controller
+		$controller = new $controller_name;
+		$action = $action_name;
+
+		if(method_exists($controller, $action))
+		{
+			// Init controller's action
+			$controller->$action();
+		}
+		else
+		{
+			Route::ErrorPage404();
+		}
+
+	}
+
+	function ErrorPage404()
+	{
+        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
+        header('HTTP/1.1 404 Not Found');
+		header("Status: 404 Not Found");
+		header('Location:'.$host.'404');
+    }
+}
