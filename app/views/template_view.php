a7c4c9564a3cbfb9fde2b5148ea9f8c0ac1b8ff4
Daniel maqq1e Hramkov <daniel.hramkov@gmail.com>
2020-04-08 12:21:07 +0500

    Add view tamplates

---
+++ app/views/template_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+	<title>Главная</title>
+</head>
+<body>
+	<?php include 'application/views/'.$content_view; ?>
+
+
+
+
+    <script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
+</body>
+</html>
